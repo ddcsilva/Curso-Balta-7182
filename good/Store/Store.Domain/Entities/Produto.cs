74e727aee13203ce9314849a3bb42c1b860ddc6d
Danilo Silva <danilo.silva@msn.com>
2023-02-23 19:14:53 -0300

    Entidade de Produto

---
+++ good/Store/Store.Domain/Entities/Produto.cs
@@ -0,0 +1,15 @@
+﻿namespace Store.Domain.Entities;
+
+public class Produto : Base
+{
+    public Produto(string titulo, decimal preco, bool ativo)
+    {
+        Titulo = titulo;
+        Preco = preco;
+        Ativo = ativo;
+    }
+
+    public string Titulo { get; private set; }
+    public decimal Preco { get; private set; }
+    public bool Ativo { get; private set; }
+}
