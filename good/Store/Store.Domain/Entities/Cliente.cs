2801381f64263d95e2b5a5dd3b2da1a2ec6d9c76
Danilo Silva <danilo.silva@msn.com>
2023-02-23 19:08:07 -0300

    Entidade do Cliente

---
+++ good/Store/Store.Domain/Entities/Cliente.cs
@@ -0,0 +1,13 @@
+﻿namespace Store.Domain.Entities;
+
+public class Cliente
+{
+    public Cliente(string nome, string email)
+    {
+        Nome = nome;
+        Email = email;
+    }
+
+    public string Nome { get; private set; }
+    public string Email { get; private set; }
+}
