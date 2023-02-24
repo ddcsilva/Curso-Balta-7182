be3f88614edfec588fc1ad1d6fe10f225c218ff2
Danilo Silva <danilo.silva@msn.com>
2023-02-24 10:25:38 -0300

    Criando as Queries

---
+++ good/Store/Store.Domain/Queries/ProdutoQueries.cs
@@ -0,0 +1,17 @@
+﻿using Store.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Store.Domain.Queries;
+
+public static class ProdutoQueries
+{
+    public static Expression<Func<Produto, bool>> GetProdutosAtivos()
+    {
+        return x => x.Ativo;
+    }
+
+    public static Expression<Func<Produto, bool>> GetProdutosInativos()
+    {
+        return x => x.Ativo == false;
+    }
+}
