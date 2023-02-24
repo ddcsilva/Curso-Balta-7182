8da95cc38940b0040ddf81f135ac73ce3747dd91
Danilo Silva <danilo.silva@msn.com>
2023-02-24 06:20:30 -0300

    Validando o Item do Pedido

--- good/Store/Store.Domain/Entities/ItemPedido.cs
+++ good/Store/Store.Domain/Entities/ItemPedido.cs
@@ -1,9 +1,18 @@
-﻿namespace Store.Domain.Entities;
+﻿using Flunt.Validations;
+
+namespace Store.Domain.Entities;
 
 public class ItemPedido : Base
 {
     public ItemPedido(Produto produto, int quantidade)
     {
+        AddNotifications(
+            new Contract()
+                .Requires()
+                .IsNotNull(produto, "Produto", "Produto Inválido")
+                .IsGreaterThan(quantidade, 0, "Quantidade", "A quantidade deve ser maior que zero")
+        );
+
         Produto = produto;
         Preco = Produto != null ? produto.Preco : 0;
         Quantidade = quantidade;
