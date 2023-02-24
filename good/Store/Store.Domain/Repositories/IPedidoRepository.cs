49b11f9babeb49061e40254066b8e953cf19eaf5
Danilo Silva <danilo.silva@msn.com>
2023-02-24 10:41:38 -0300

    Criando os Repositórios

---
+++ good/Store/Store.Domain/Repositories/IPedidoRepository.cs
@@ -0,0 +1,8 @@
+﻿using Store.Domain.Entities;
+
+namespace Store.Domain.Repositories;
+
+public interface IPedidoRepository
+{
+    void Salvar(Pedido pedido);
+}
---
+++ good/Store/Store.Domain/Repositories/ITaxaEntregaRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Store.Domain.Repositories;
+
+public interface ITaxaEntregaRepository
+{
+    decimal Get(string cep);
+}
