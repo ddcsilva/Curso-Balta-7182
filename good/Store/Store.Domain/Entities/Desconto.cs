5880de8f5acb6db1b549d9f0171f404ae505d567
Danilo Silva <danilo.silva@msn.com>
2023-02-24 05:28:14 -0300

    Entidade de Desconto

---
+++ good/Store/Store.Domain/Entities/Desconto.cs
@@ -0,0 +1,26 @@
+﻿namespace Store.Domain.Entities;
+
+public class Desconto : Base
+{
+    public Desconto(decimal quantia, DateTime dataExpiracao)
+    {
+        Quantia = quantia;
+        DataExpiracao = dataExpiracao;
+    }
+
+    public decimal Quantia { get; private set; }
+    public DateTime DataExpiracao { get; private set; }
+
+    public bool EstaValido()
+    {
+        return DateTime.Compare(DateTime.Now, DataExpiracao) < 0;
+    }
+
+    public decimal Valor()
+    {
+        if (EstaValido())
+            return Quantia;
+        else
+            return 0;
+    }
+}
