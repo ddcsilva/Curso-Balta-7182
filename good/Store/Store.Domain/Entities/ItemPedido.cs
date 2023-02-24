716ad8ca44b239b5a4f73ed4942a6153b82a81f4
Danilo Silva <danilo.silva@msn.com>
2023-02-24 05:36:07 -0300

    Arrumando namespace

--- good/Store/Store.Domain/Entities/ItemPedido.cs
+++ good/Store/Store.Domain/Entities/ItemPedido.cs
@@ -1,21 +1,20 @@
-﻿namespace Store.Domain.Entities
+﻿namespace Store.Domain.Entities;
+
+public class ItemPedido : Base
 {
-    public class ItemPedido : Base
+    public ItemPedido(Produto produto, int quantidade)
     {
-        public ItemPedido(Produto produto, int quantidade)
-        {
-            Produto = produto;
-            Preco = Produto != null ? produto.Preco : 0;
-            Quantidade = quantidade;
-        }
+        Produto = produto;
+        Preco = Produto != null ? produto.Preco : 0;
+        Quantidade = quantidade;
+    }
 
-        public Produto Produto { get; private set; }
-        public decimal Preco { get; private set; }
-        public int Quantidade { get; private set; }
+    public Produto Produto { get; private set; }
+    public decimal Preco { get; private set; }
+    public int Quantidade { get; private set; }
 
-        public decimal Total()
-        {
-            return Preco + Quantidade;
-        }
+    public decimal Total()
+    {
+        return Preco + Quantidade;
     }
 }
