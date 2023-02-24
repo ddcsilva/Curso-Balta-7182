f15b71d2d1b2da4738baa6f6bb959546fe190391
Danilo Silva <danilo.silva@msn.com>
2023-02-24 11:28:38 -0300

    Commands do Pedido

---
+++ good/Store/Store.Domain/Commands/CriarItemPedidoCommand.cs
@@ -0,0 +1,29 @@
+﻿using Store.Domain.Commands.Interfaces;
+using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace Store.Domain.Commands
+{
+    public class CriarItemPedidoCommand : Notifiable, ICommand
+    {
+        public CriarItemPedidoCommand() { }
+
+        public CriarItemPedidoCommand(Guid produto, int quantidade)
+        {
+            Produto = produto;
+            Quantidade = quantidade;
+        }
+
+        public Guid Produto { get; set; }
+        public int Quantidade { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+                .Requires()
+                .HasLen(Produto.ToString(), 32, "Produto", "Produto inválido")
+                .IsGreaterThan(Quantidade, 0, "Quantidade", "Quantidade inválida")
+            );
+        }
+    }
+}
---
+++ good/Store/Store.Domain/Commands/CriarPedidoCommand.cs
@@ -0,0 +1,36 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using Store.Domain.Commands.Interfaces;
+
+namespace Store.Domain.Commands
+{
+    public class CriarPedidoCommand : Notifiable, ICommand
+    {
+        public CriarPedidoCommand()
+        {
+            Items = new List<CriarItemPedidoCommand>();
+        }
+
+        public CriarPedidoCommand(string cliente, string cep, string codigoPromocional, IList<CriarItemPedidoCommand> items)
+        {
+            Cliente = cliente;
+            Cep = cep;
+            this.codigoPromocional = codigoPromocional;
+            Items = items;
+        }
+
+        public string Cliente { get; set; }
+        public string Cep { get; set; }
+        public string codigoPromocional { get; set; }
+        public IList<CriarItemPedidoCommand> Items { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+                .Requires()
+                .HasLen(Cliente, 11, "Cliente", "Cliente inválido")
+                .HasLen(Cep, 8, "Cep", "CEP inválido")
+            );
+        }
+    }
+}
