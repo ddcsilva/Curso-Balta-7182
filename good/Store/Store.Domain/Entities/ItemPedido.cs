843af2613a135b50413c737ae354dabadd8a3d9f
Danilo Silva <danilo.silva@msn.com>
2023-02-24 05:35:19 -0300

    Entidade ItemPedido

---
+++ good/Store/Store.Domain/Entities/ItemPedido.cs
@@ -0,0 +1,21 @@
+﻿namespace Store.Domain.Entities
+{
+    public class ItemPedido : Base
+    {
+        public ItemPedido(Produto produto, int quantidade)
+        {
+            Produto = produto;
+            Preco = Produto != null ? produto.Preco : 0;
+            Quantidade = quantidade;
+        }
+
+        public Produto Produto { get; private set; }
+        public decimal Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        public decimal Total()
+        {
+            return Preco + Quantidade;
+        }
+    }
+}
